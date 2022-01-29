ad38d1617bb7581a6c7aa69ba9fc99bcf3a23ac1
Mario <marcae7@gmail.com>
2022-01-28 23:51:51 -0500

    drop subtitle

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,17 +35,14 @@ function Page({data}: Data) {
             </Head>
             <header>
                 <div className="flex flex-col items-center gap-3 mx-5 py-10">
-                    <h3 className="text-5xl font-custom1 text-red-600">
+                    <h3 className="text-5xl font-custom1 text-blue-400">
                         Hello world,
                     </h3>
                     <h3 className="text-5xl font-custom1 text-green-600">
-                        My Name is Mario
+                        I'm Mario
                     </h3>
-                    <h3 className="text-4xl font-custom1 text-yellow-300">
-                        I'm a Senior Web developer!
-                    </h3>
-                    <h3 className="text-2xl font-custom2 text-green-100 mt-5">
-                        I make full stack web integrations with next generation technologies!
+                    <h3 className="text-2xl font-custom1 text-yellow-300">
+                        I'm a data Scientist and Senior Web developer!
                     </h3>
                     <div className={"w-screen font-custom1 px-10 sm:px-40 mt-10 text-xs"}>
                         <TreeChart data={{name: "Me", children: data}}/>
